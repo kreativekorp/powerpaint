0c8dbc52899593208491364c4f6c50d2657f965e
Rebecca G. Bettencourt <beckiejo@gmail.com>
2015-07-18 21:03:12 -0700

    Add arcTo support to stroke paths.

--- main/java/PowerPaint/src/com/kreative/paint/stroke/ArrowheadShape.java
+++ main/java/PowerPaint/src/com/kreative/paint/stroke/ArrowheadShape.java
@@ -5,9 +5,10 @@ import java.awt.Rectangle;
 import java.awt.Shape;
 import java.awt.Stroke;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Arc2D;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.GeneralPath;
 import java.awt.geom.Line2D;
-import java.awt.geom.Path2D;
 import java.awt.geom.PathIterator;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
@@ -150,7 +151,7 @@ public abstract class ArrowheadShape implements Shape {
 		}
 		// Do not try this at home.
 		private Path(Object[] o, EndCap endCap, LineJoin lineJoin, float miterLimit, boolean stroke, boolean fill) {
-			super((Path2D.Float)o[1], stroke, fill);
+			super((GeneralPath)o[1], stroke, fill);
 			this.d = (String)o[0];
 			this.endCap = endCap;
 			this.lineJoin = lineJoin;
@@ -232,8 +233,8 @@ public abstract class ArrowheadShape implements Shape {
 			for (float point : points) f += point;
 			return Float.floatToIntBits(f);
 		}
-		private static Path2D.Float makeShape(float[] points) {
-			Path2D.Float p = new Path2D.Float();
+		private static GeneralPath makeShape(float[] points) {
+			GeneralPath p = new GeneralPath();
 			if (points.length >= 2) {
 				p.moveTo(points[0], points[1]);
 				for (int i = 2, j = 1, n = points.length / 2; j < n; j++) {
@@ -296,8 +297,8 @@ public abstract class ArrowheadShape implements Shape {
 			for (float point : points) f += point;
 			return Float.floatToIntBits(f);
 		}
-		private static Path2D.Float makeShape(float[] points) {
-			Path2D.Float p = new Path2D.Float();
+		private static GeneralPath makeShape(float[] points) {
+			GeneralPath p = new GeneralPath();
 			if (points.length >= 2) {
 				p.moveTo(points[0], points[1]);
 				for (int i = 2, j = 1, n = points.length / 2; j < n; j++) {
@@ -420,8 +421,11 @@ public abstract class ArrowheadShape implements Shape {
 	private static final Pattern INSTRUCTION_PATTERN = Pattern.compile("[A-Za-z]|([+-]?)([0-9]+([.][0-9]*)?|[.][0-9]+)");
 	private static Object[] parseInstructions(String s) {
 		List<String> instructions = new ArrayList<String>();
-		Path2D p = new Path2D.Float();
-		float lx = 0.0f, ly = 0.0f, cx1, cy1, cx2, cy2;
+		GeneralPath p = new GeneralPath();
+		float lx = 0.0f, ly = 0.0f;
+		float cx1, cy1, cx2, cy2;
+		float arx, ary, a;
+		boolean large, sweep;
 		Matcher m = INSTRUCTION_PATTERN.matcher(s);
 		while (m.find()) {
 			String inst = m.group();
@@ -506,6 +510,28 @@ public abstract class ArrowheadShape implements Shape {
 					instructions.add(Float.toString(ly += parseInstructionFloat(m)));
 					p.curveTo(cx1, cy1, cx2, cy2, lx, ly);
 					break;
+				case 'A':
+					instructions.add("A");
+					instructions.add(Float.toString(arx = parseInstructionFloat(m)));
+					instructions.add(Float.toString(ary = parseInstructionFloat(m)));
+					instructions.add(Float.toString(a = parseInstructionFloat(m)));
+					instructions.add((large = (parseInstructionFloat(m) != 0)) ? "1" : "0");
+					instructions.add((sweep = (parseInstructionFloat(m) != 0)) ? "1" : "0");
+					instructions.add(Float.toString(lx = parseInstructionFloat(m)));
+					instructions.add(Float.toString(ly = parseInstructionFloat(m)));
+					arcTo(p, arx, ary, a, large, sweep, lx, ly);
+					break;
+				case 'a':
+					instructions.add("A");
+					instructions.add(Float.toString(arx = parseInstructionFloat(m)));
+					instructions.add(Float.toString(ary = parseInstructionFloat(m)));
+					instructions.add(Float.toString(a = parseInstructionFloat(m)));
+					instructions.add((large = (parseInstructionFloat(m) != 0)) ? "1" : "0");
+					instructions.add((sweep = (parseInstructionFloat(m) != 0)) ? "1" : "0");
+					instructions.add(Float.toString(lx += parseInstructionFloat(m)));
+					instructions.add(Float.toString(ly += parseInstructionFloat(m)));
+					arcTo(p, arx, ary, a, large, sweep, lx, ly);
+					break;
 				case 'Z':
 				case 'z':
 					instructions.add("Z");
@@ -535,5 +561,72 @@ public abstract class ArrowheadShape implements Shape {
 		} else {
 			return 0.0f;
 		}
+	}
+	private static void arcTo(
+		GeneralPath p, double rx, double ry, double a,
+		boolean large, boolean sweep, double x, double y
+	) {
+		Point2D p0 = p.getCurrentPoint();
+		double x0 = p0.getX();
+		double y0 = p0.getY();
+		if (x0 == x && y0 == y) return;
+		if (rx == 0 || ry == 0) { p.lineTo(x, y); return; }
+		double dx2 = (x0 - x) / 2;
+		double dy2 = (y0 - y) / 2;
+		a = Math.toRadians(a % 360);
+		double ca = Math.cos(a);
+		double sa = Math.sin(a);
+		double x1 = sa * dy2 + ca * dx2;
+		double y1 = ca * dy2 - sa * dx2;
+		rx = Math.abs(rx);
+		ry = Math.abs(ry);
+		double Prx = rx * rx;
+		double Pry = ry * ry;
+		double Px1 = x1 * x1;
+		double Py1 = y1 * y1;
+		double rc = Px1/Prx + Py1/Pry;
+		if (rc > 1) {
+			rx = Math.sqrt(rc) * rx;
+			ry = Math.sqrt(rc) * ry;
+			Prx = rx * rx;
+			Pry = ry * ry;
+		}
+		double s = (large == sweep) ? -1 : 1;
+		double sq = ((Prx*Pry)-(Prx*Py1)-(Pry*Px1)) / ((Prx*Py1)+(Pry*Px1));
+		if (sq < 0) sq = 0;
+		double m = s * Math.sqrt(sq);
+		double cx1 = m *  ((rx * y1) / ry);
+		double cy1 = m * -((ry * x1) / rx);
+		double sx2 = (x0 + x) / 2;
+		double sy2 = (y0 + y) / 2;
+		double cx = sx2 + ca * cx1 - sa * cy1;
+		double cy = sy2 + sa * cx1 + ca * cy1;
+		double ux = (x1 - cx1) / rx;
+		double uy = (y1 - cy1) / ry;
+		double vx = (-x1 -cx1) / rx;
+		double vy = (-y1 -cy1) / ry;
+		double sn = Math.sqrt(ux*ux + uy*uy);
+		double sp = ux;
+		double ss = (uy < 0) ? -1 : 1;
+		double as = Math.toDegrees(ss * Math.acos(sp / sn));
+		double en = Math.sqrt((ux*ux + uy*uy) * (vx*vx + vy*vy));
+		double ep = ux * vx + uy * vy;
+		double es = (ux * vy - uy * vx < 0) ? -1 : 1;
+		double ae = Math.toDegrees(es * Math.acos(ep / en));
+		if (!sweep && ae > 0) ae -= 360;
+		if (sweep && ae < 0) ae += 360;
+		ae %= 360;
+		as %= 360;
+		Arc2D.Double arc = new Arc2D.Double();
+		arc.x = cx - rx;
+		arc.y = cy - ry;
+		arc.width = rx * 2;
+		arc.height = ry * 2;
+		arc.start = -as;
+		arc.extent = -ae;
+		double acx = arc.getCenterX();
+		double acy = arc.getCenterY();
+		AffineTransform t = AffineTransform.getRotateInstance(a, acx, acy);
+		p.append(t.createTransformedShape(arc), true);
 	}
 }
