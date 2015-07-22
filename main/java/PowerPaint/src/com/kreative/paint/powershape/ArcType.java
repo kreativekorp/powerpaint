c5d3a1755c6ac68359cf8cc22e736e21a1d7aa6f
Rebecca G. Bettencourt <beckiejo@gmail.com>
2015-07-21 23:19:16 -0700

    Adding new PowerShape implementation before removing the legacy one.

---
+++ main/java/PowerPaint/src/com/kreative/paint/powershape/ArcType.java
@@ -0,0 +1,24 @@
+package com.kreative.paint.powershape;
+
+import java.awt.geom.Arc2D;
+
+public enum ArcType {
+	OPEN(Arc2D.OPEN),
+	CHORD(Arc2D.CHORD),
+	PIE(Arc2D.PIE);
+	
+	public final int awtValue;
+	
+	private ArcType(int awtValue) {
+		this.awtValue = awtValue;
+	}
+	
+	public static ArcType forAWTValue(int value) {
+		switch (value) {
+		case Arc2D.OPEN: return OPEN;
+		case Arc2D.CHORD: return CHORD;
+		case Arc2D.PIE: return PIE;
+		default: return null;
+		}
+	}
+}
---
+++ main/java/PowerPaint/src/com/kreative/paint/powershape/WindingRule.java
@@ -0,0 +1,22 @@
+package com.kreative.paint.powershape;
+
+import java.awt.geom.GeneralPath;
+
+public enum WindingRule {
+	EVEN_ODD(GeneralPath.WIND_EVEN_ODD),
+	NON_ZERO(GeneralPath.WIND_NON_ZERO);
+	
+	public final int awtValue;
+	
+	private WindingRule(int awtValue) {
+		this.awtValue = awtValue;
+	}
+	
+	public static WindingRule forAWTValue(int value) {
+		switch (value) {
+		case GeneralPath.WIND_EVEN_ODD: return EVEN_ODD;
+		case GeneralPath.WIND_NON_ZERO: return NON_ZERO;
+		default: return null;
+		}
+	}
+}
